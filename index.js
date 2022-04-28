2783a037aa116432537982ab63724a64a69929dd
Abram Shook <ajshook77@gmail.com>
2022-04-27 19:18:26 -0500

    commit tree 56243a905994f56c5cf2d902a454984d2d2a1168

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// ex. const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+// create a function that uses the .find method called superbowlWin()**
+// the function should receive 1 argument which is an Array of Javascript Objects**
+// each object has 2 properties (year, result)
+// it should use find() to test each Object to see if the result is ==='W'**
+// it should return a year when the win occured
+// if no win is found it should return undefined
+
+const superbowlWin = function (arr) {
+  let winnings = arr.find((obj) => obj.result === 'W')
+  if (winnings) return winnings.year
+  return undefined
+}
